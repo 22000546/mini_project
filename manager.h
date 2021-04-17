03c7bde87e308a2ad35b8b42c0eb12fe2f50df7e
Yeeun <22000546@handong.edu>
2021-04-18 05:16:02 +0900

    Update manager.h : Correct typos

--- manager.h
+++ manager.h
@@ -1,6 +1,6 @@
 int listProduct(Product *p, int index); //등록된 상품을 index만큼 보여주는 함수
-void searchProductName(Product *p, in index); //상품 이름으로 검색하는 함수
-void searchProductWeight(Product *p, in index); //상품 무게로 검색하는 함수
-void searchProductPrice(Product *p, in index); //상품 가격대로 검색하는 함수
+void searchProductName(Product *p, int index); //상품 이름으로 검색하는 함수
+void searchProductWeight(Product *p, int index); //상품 무게로 검색하는 함수
+void searchProductPrice(Product *p, int index); //상품 가격대로 검색하는 함수
 int saveData(Product *p, int index); //파일 저장 함수
 int loadData(Product *p); //파일을 불러오는 함수
