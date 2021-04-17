0a89146db2b72e46b398a09ee50c4d009e628f5c
Yeeun <22000546@handong.edu>
2021-04-18 04:52:15 +0900

    Update manager.c : Define void searchProductWeight(Product *p, int index)

--- manager.c
+++ manager.c
@@ -34,6 +34,24 @@ void searchProductName(Product *p, int index) {
 		printf("\n%s\n에 대한 검색 결과가 없습니다.\n", search);
 }
 
+void searchProductWeight(Product *p, int index) {
+	int searchcnt = 0;
+	char search[50];
+
+	getchar();
+	printf("검색하고 싶은 상품의 무게 범위를 선택해주세요. :");
+	printf("1: 0~100\t2: 101~200\t3: 201~300\t4: 301~400\t...");
+	scanf("%d", search);
+	for(int i = 0; i < index; i ++) {
+		if((p[i].weight > (search-1)*100)&&(p[i].weight <= search*100)) {
+			readProduct(p[i]);
+			searchcnt ++;
+		}
+	}
+	if(searchcnt == 0)
+		printf("\n%s\n에 대한 검색 결과가 없습니다.\n", search);
+}
+
 int saveData(Product *p, int index) {
 	FILE *fp = fopen("product.txt", "wt");
 	fprintf(fp, "%s %d %d %d %d\n", p->name, p->weight, p->price, p->stars, p->numStars);
