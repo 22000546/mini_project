d0929124f7b4455cbb561086d34a2cbd4714246b
Yeeun <22000546@handong.edu>
2021-04-18 05:06:50 +0900

    Update manager.h : Declare int loadData(Product *p)

--- manager.h
+++ manager.h
@@ -3,3 +3,4 @@ void searchProductName(Product *p, in index); //상품 이름으로 검색하는
 void searchProductWeight(Product *p, in index); //상품 무게로 검색하는 함수
 void searchProductPrice(Product *p, in index); //상품 가격대로 검색하는 함수
 int saveData(Product *p, int index); //파일 저장 함수
+int loadData(Product *p); //파일을 불러오는 함수
