17aeb4142b7a25024ebb536b107053440f5bfd1f
Yeeun <22000546@handong.edu>
2021-04-18 04:44:05 +0900

    Updata manager.h : Declare void searchProductName(Product *p, int index), void searchProductWeight(Product *p, int index), void searchProductPrice(Product *p, int index)

--- manager.h
+++ manager.h
@@ -1,2 +1,5 @@
 int listProduct(Product *p, int index); //등록된 상품을 index만큼 보여주는 함수
+void searchProductName(Product *p, in index); //상품 이름으로 검색하는 함수
+void searchProductWeight(Product *p, in index); //상품 무게로 검색하는 함수
+void searchProductPrice(Product *p, in index); //상품 가격대로 검색하는 함수
 int saveData(Product *p, int index); //파일 저장 함수
