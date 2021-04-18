64f11fd6b61de6dabe9c1ebe341a378e5f3beead
Yeeun <22000546@handong.edu>
2021-04-18 16:15:51 +0900

    Update manager.h : Declare int selectMenu()

--- manager.h
+++ manager.h
@@ -4,3 +4,4 @@ void searchProductWeight(Product *p, int index); //상품 무게로 검색하는
 void searchProductPrice(Product *p, int index); //상품 가격대로 검색하는 함수
 int saveData(Product *p, int index); //파일 저장 함수
 int loadData(Product *p); //파일을 불러오는 함수
+int selectMenu(); //메뉴를 선택하는 함수
