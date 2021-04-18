f82e0a36cb15ee950b611ccec78ec2c64ab5c0fc
Yeeun <22000546@handong.edu>
2021-04-18 16:48:19 +0900

    Update main.c : Implement menu 1 (add a product)

--- main.c
+++ main.c
@@ -12,41 +12,20 @@ int main() {
 	Product plist[10];
 	int index = 0;
 	int count = 0;
+	int menu;
+	int checker;
 
-	//test CRUD
-	for(int i = 0; i < 5; i ++) {
-	      if(createProduct(&plist[i]) == 1) {
-		      index ++;
-		      count ++;
-		      printf("=> createProduct %d번 정상 작동\n", i+1);
-	      }
-	}
-
-	if(listProduct(plist, index) == 1)
-		printf("=> listProduct 정상 작동 \n");	
-	int no;
-	printf("수정할 번호 선택 : ");
-	scanf("%d", &no);
-	if((no > 0)&&(no <= count)) {
-		if(updateProduct(&plist[no-1]) == 1) 
-			printf("updateProduct 정상 작동\n");
-	} else {
-		printf("번호가 잘못되었습니다. \n");
-	}
-	printf("삭제할 번호 선택 : ");
-	scanf("%d", &no);
-	if((no > 0)&&(no <= count)) {
-		if(deleteProduct(&plist[no-1]) == 1) { 
-			count --;
-			printf("deleteProduct 정상 작동\n");
+	while(1) {
+		menu = selectMenu();
+		if(menu == 0)
+			break;
+		else if(menu == 1) {
+			checker = createProduct(&plist[index++]);
+			count ++;
+			if(checker == 1)
+				printf("=> 제품이 성공적으로 등록되었습니다.\n\n");
 		}
 	}
-	else {
-		printf("번호가 잘못되었습니다.\n");
-	}
-
-	printf("최종 제품 리스트입니다.\n");
-	listProduct(plist, index);
 
 	return 0;
 }
