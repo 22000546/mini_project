e4c940da6d1e0a9db23cdcd7b00929fe8e4db165
Yeeun <22000546@handong.edu>
2021-04-18 20:09:58 +0900

    Update manager.c : Revise int saveData(Product *p, int index)

--- manager.c
+++ manager.c
@@ -72,7 +72,7 @@ void searchProductPrice(Product *p, int index) {
 int saveData(Product *p, int index) {
 	FILE *fp = fopen("product.txt", "wt");
 	for(int i = 0; i < index; i ++) {
-		if(strcmp(p[i].name, "noname"))
+		if(strcmp(p[i].name, "noname") == 0)
 			continue;
 		fprintf(fp, "%s %d %d %d %d\n", p[i].name, p[i].weight, p[i].price, p[i].stars, p[i].numStars);
 	}
